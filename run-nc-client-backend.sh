ac2f25a25e4274905da3d241197335788af4e528
MartinCristiano <imcristianomartin@gmail.com>
2018-09-28 15:53:36 +0200

    fix run nc client

--- run-nc-client-backend.sh
+++ run-nc-client-backend.sh
@@ -11,16 +11,16 @@ IPNCServer=192.168.18.142
 
 # echo -e "${COLOR}\n>> Now the shineNCnetwork overlay network will be created.${NC}"
 # docker network create shineNCnetwork
-rm -rf /home/opensand/Desktop/shine-scripts/ConfigFile/clt 
-rm -rf /home/opensand/Desktop/shine-scripts/mpd
-rm -rf /home/opensand/Desktop/shine-scripts/mlst
-cp -R /home/opensand/Desktop/clt /home/opensand/Desktop/shine-scripts/ConfigFile/clt
-cp -R  /home/opensand/Desktop/mpd /home/opensand/Desktop/shine-scripts/mpd
-cp -R /home/opensand/Desktop/mlst /home/opensand/Desktop/shine-scripts/mlst
-cp -R /home/opensand/Desktop/id_rsa.pub /home/opensand/Desktop/shine-scripts/
+rm -rf /home/opensand/Desktop/shine-testbed/ConfigFile/clt 
+rm -rf /home/opensand/Desktop/shine-testbed/mpd
+rm -rf /home/opensand/Desktop/shine-testbed/mlst
+cp -R /home/opensand/Desktop/clt /home/opensand/Desktop/shine-testbed/ConfigFile/clt
+cp -R  /home/opensand/Desktop/mpd /home/opensand/Desktop/shine-testbed/mpd
+cp -R /home/opensand/Desktop/mlst /home/opensand/Desktop/shine-testbed/mlst
+cp -R /home/opensand/Desktop/id_rsa.pub /home/opensand/Desktop/shine-testbed/
 echo -e "${COLOR}\n>> Running shine storage"
 
-docker run -d --name shine-storage -p 8686:8686 -p 8687:8687 -p 9601:9601 -p 8690:8690 -p 8688:8688 --mount type=bind,source=/home/opensand/Desktop/shine-scripts/mpd,target=/usr/src/app/mpd --mount type=bind,source=/home/opensand/Desktop/shine-scripts/mlst,target=/usr/src/app/mlst esashineproject/shine-storage
+docker run -d --name shine-storage -p 8686:8686 -p 8687:8687 -p 9601:9601 -p 8690:8690 -p 8688:8688 --mount type=bind,source=/home/opensand/Desktop/shine-testbed/mpd,target=/usr/src/app/mpd --mount type=bind,source=/home/opensand/Desktop/shine-testbed/mlst,target=/usr/src/app/mlst esashineproject/shine-storage
 
 STORAGE= docker inspect -f "{{ .NetworkSettings.IPAddress }}" shine-storage
 
