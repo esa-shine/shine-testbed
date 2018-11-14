487326b1c5ba1d20df3a730e06fd0393f728747e
cris <imcristianomartin@gmail.com>
2018-11-14 11:02:38 +0000

    adding box configuration

---
+++ run-shine-front-end.box.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+BOLD='\033[1;32m'
+COLOR='\033[0;32m'
+NC='\033[0m'
+
+
+
+echo -e "${COLOR}\n>> Now Running Shine frontend spring boot container.${NC}"
+docker run -d --name shine-spring -p 8443:8443 -e "PROFILE=box" esashineproject/shine-frontend
+
+
+echo -e "${COLOR}\n>> Now Running Janus container.${NC}"
+
+docker run -d --name janus --network host  esashineproject/janus
