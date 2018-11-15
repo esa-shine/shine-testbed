3cb69975b4a2484001c897cc52a6ba3f030d8cb9
Fabio Giangrande <f.giangrande@studenti.unina.it>
2018-11-15 13:33:50 +0000

    fix run nc-server

--- run-nc-server.sh
+++ run-nc-server.sh
@@ -35,7 +35,7 @@ pass="XCcube"
 
 sshpass -p "$pass" scp id_rsa.pub  opensand@192.168.19.101:/home/opensand/Desktop
 
-sshpass -p "$pass" scp -P 19000 id_rsa.pub  opensand@192.168.19.110:/home/opensand/Desktop
+sshpass -p "$pass" scp id_rsa.pub  opensand@192.168.19.110:/home/opensand/Desktop
 
 
 
