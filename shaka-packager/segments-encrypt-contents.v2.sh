292a74a91f82b718da2ab68e7f20effc494ca98d
Fabio Giangrande <f.giangrande@studenti.unina.it>
2018-11-21 14:16:46 +0000

    fix

--- shaka-packager/segments-encrypt-contents.v2.sh
+++ shaka-packager/segments-encrypt-contents.v2.sh
@@ -16,14 +16,14 @@ NOME_VIDEO=$(echo $i | cut -f 1 -d ".")
 	then 
 		echo "non e' stata inserita la durata di segmenti! Di default 10 s. Creo i contenuti cifrati..."
 		
-		packager input=/shaka_packager/media/mp4/$NOME_VIDEO.mp4,stream=audio,output=/storagedash-s2/$NOME_VIDEO-Audio.mp4 input=/shaka_packager/media/mp4/$NOME_VIDEO.mp4,stream=video,output=/storagedash-s2/$NOME_VIDEO-Video.mp4 --profile on-demand --enable_widevine_encryption --key_server_url "https://license.uat.widevine.com/cenc/getcontentkey/widevine_test" --content_id "3031323334353637" --signer "widevine_test" --aes_signing_key "1ae8ccd0e7985cc0b6203a55855a1034afc252980e970ca90e5202689f947ab9" --aes_signing_iv "d58ce954203b7c9a9a9d467f59839249" --mpd_output mpd/$NOME_VIDEO.mpd
+		packager input=/shaka_packager/media/mp4/$NOME_VIDEO.mp4,stream=audio,output=/storagedash-s2/$NOME_VIDEO-Audio.mp4 input=/shaka_packager/media/mp4/$NOME_VIDEO.mp4,stream=video,output=/storagedash-s2/$NOME_VIDEO-Video.mp4 --profile on-demand --enable_widevine_encryption --key_server_url "https://license.uat.widevine.com/cenc/getcontentkey/widevine_test" --content_id "3031323334353637" --signer "widevine_test" --aes_signing_key "1ae8ccd0e7985cc0b6203a55855a1034afc252980e970ca90e5202689f947ab9" --aes_signing_iv "d58ce954203b7c9a9a9d467f59839249" --mpd_output mpd-s2/$NOME_VIDEO.mpd
 
 		echo -e "Contenuto $NOME_VIDEO.mp4 cifrato correttamente! Le tracce audio/video cifrate sono $NOME_VIDEO-Audio.mp4 e $NOME_VIDEO-Video.mp4. L'mpd da mettere nel frontend e' $NOME_VIDEO.mpd"
 		
 	else 
 		echo "e' stata inserita la durata di segmenti= $SEGMENT_DURATION. Creo i contenuti cifrati..."
 		
-		packager input=/shaka_packager/media/mp4/$NOME_VIDEO.mp4,stream=audio,output=/storagedash-s2/$NOME_VIDEO-Audio-$SEGMENT_DURATION-s.mp4 input=/shaka_packager/media/mp4/$NOME_VIDEO.mp4,stream=video,output=/storagedash-s2/$NOME_VIDEO-Video-$SEGMENT_DURATION-s.mp4 --profile on-demand --enable_widevine_encryption --segment_duration $SEGMENT_DURATION --fragment_duration $SEGMENT_DURATION  --key_server_url "https://license.uat.widevine.com/cenc/getcontentkey/widevine_test" --content_id "3031323334353637" --signer "widevine_test" --aes_signing_key "1ae8ccd0e7985cc0b6203a55855a1034afc252980e970ca90e5202689f947ab9" --aes_signing_iv "d58ce954203b7c9a9a9d467f59839249" --mpd_output mpd/$NOME_VIDEO-$SEGMENT_DURATION-s.mpd
+		packager input=/shaka_packager/media/mp4/$NOME_VIDEO.mp4,stream=audio,output=/storagedash-s2/$NOME_VIDEO-Audio-$SEGMENT_DURATION-s.mp4 input=/shaka_packager/media/mp4/$NOME_VIDEO.mp4,stream=video,output=/storagedash-s2/$NOME_VIDEO-Video-$SEGMENT_DURATION-s.mp4 --profile on-demand --enable_widevine_encryption --segment_duration $SEGMENT_DURATION --fragment_duration $SEGMENT_DURATION  --key_server_url "https://license.uat.widevine.com/cenc/getcontentkey/widevine_test" --content_id "3031323334353637" --signer "widevine_test" --aes_signing_key "1ae8ccd0e7985cc0b6203a55855a1034afc252980e970ca90e5202689f947ab9" --aes_signing_iv "d58ce954203b7c9a9a9d467f59839249" --mpd_output mpd-s2/$NOME_VIDEO-$SEGMENT_DURATION-s.mpd
 
 		echo -e "\n Contenuto $NOME_VIDEO.mp4 cifrato correttamente! Le tracce audio/video cifrate sono $NOME_VIDEO-Audio-$SEGMENT_DURATION-s.mp4 e $NOME_VIDEO-Video-$SEGMENT_DURATION-s.mp4. L'mpd da mettere nel frontend e' $NOME_VIDEO-$SEGMENT_DURATION-s.mpd"
  fi
